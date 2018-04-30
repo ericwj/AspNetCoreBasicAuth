8bc28d15467f0ddb4c0da87baf89c4a9894c2e66
Eric <eric.jonker@live.nl>
2018-04-30 21:44:51 +0200

    Downgrade to SDK 2.1.4/runtime 2.0.7

--- samples/BasicAuthentication/Startup.cs
+++ samples/BasicAuthentication/Startup.cs
@@ -25,7 +25,6 @@ namespace BasicAuthentication
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
@@ -43,7 +42,7 @@ namespace BasicAuthentication
                     });
                 });
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
